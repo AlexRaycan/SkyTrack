e2b25d52c6dcde537ea7a75cda996e3a59d70c1a
Alex Ray <103534203+AlexRaycan@users.noreply.github.com>
2025-07-01 22:19:30 +0700

    Обновление логики и компонентов интерфейса
    - **[feature]** Упрощение логики компонентов `Header` и `HeaderMenuItem` для сокращения дублирования кода.
    - **[feature]** Создание новой конфигурации данных меню в файле `Header-menu.data.ts`.
    - **[feature]** Добавление стандартных иконок в логике компонента `HeaderMenuItem`.
    - **[feature]** Обновление логики кнопки в `HeaderMenuItem` для работы с адаптивностью через `useWindowWidth`.
    - **[bugfix]** Исправление импорта в файле `src/routeTree.gen.ts`.
    - **[bugfix]** Устранение лишних импорта и полей в файловой структуре меню.

--- src/components/Header/Header-menu.data.ts
+++ src/components/Header/Header-menu.data.ts
@@ -1,34 +1,19 @@
-import { UserRound } from '@components/animate-ui/icons/user-round.tsx';
-import { Heart } from '@components/animate-ui/icons/heart.tsx';
-import MoreIcon from '@assets/icons/other/more.svg?react';
-import Logo from '@assets/logo/logo.svg?react';
-import type { ReactNode } from 'react';
-
 export interface IHeaderMenuItemProps {
 	label: string;
 	to: string;
-	icon: ReactNode;
 }
 
 export const HEADER_MENU: IHeaderMenuItemProps[] = [
 	{
 		label: 'Home',
 		to: '/',
-		icon: Logo,
 	},
 	{
 		label: 'Profile',
 		to: '/profile',
-		icon: UserRound,
 	},
 	{
 		label: 'Favorites',
 		to: '/favorites',
-		icon: Heart,
-	},
-	{
-		label: 'More',
-		to: '/more',
-		icon: MoreIcon,
 	},
 ];
--- src/components/Header/HeaderMenuItem/HeaderMenuItem.tsx
+++ src/components/Header/HeaderMenuItem/HeaderMenuItem.tsx
@@ -1,7 +1,14 @@
-import { cn } from '@/helpers/classNames';
 import Button from '@components/Button';
 import { Link } from '@tanstack/react-router';
 import type { IHeaderMenuItemProps } from '@components/Header/Header-menu.data.ts';
+import { cn } from '@/lib/utils.ts';
+import { useWindowWidth } from '@/hooks/useWindowWidth.ts';
+import type { ReactElement } from 'react';
+
+import { UserRound } from '@components/animate-ui/icons/user-round.tsx';
+import { Heart } from '@components/animate-ui/icons/heart.tsx';
+import MoreIcon from '@assets/icons/other/more.svg?react';
+import Logo from '@assets/logo/logo.svg?react';
 
 interface HeaderMenuItemProps {
 	className?: string;
@@ -10,19 +17,32 @@ interface HeaderMenuItemProps {
 
 const HeaderMenuItem = (props: HeaderMenuItemProps) => {
 	const { className, item } = props;
+	const { windowWidth, breakpoints } = useWindowWidth();
+	const isMobile = windowWidth < breakpoints.md; // Сравнение с брейкпоинтом 'md'
+
+	const defaultIcons: { [key: string]: ReactElement } = {
+		Profile: <UserRound />,
+		Favorites: <Heart />,
+		More: <MoreIcon />,
+		Home: <Logo />,
+	};
 
 	return (
 		<Link
 			to={item.to}
 			className={cn('inline-flex flex-1 justify-center md:flex-none', className)}
-			activeProps={linkActiveStyle}
+			activeProps={{
+				style: {
+					color: 'var(--color-accent)',
+				},
+			}}
 		>
 			<Button
 				title={'Profile'}
 				size={'large'}
 				isHorizontal={!isMobile}
 				isTransparent
-				icon={item.icon}
+				icon={defaultIcons[item.label]}
 			>
 				{item.label}
 			</Button>
