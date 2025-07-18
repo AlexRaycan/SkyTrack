70ea4a808071457961ed2697922d98bbf21239db
Alex Ray <103534203+AlexRaycan@users.noreply.github.com>
2025-07-18 23:51:21 +0700

    # Добавление расчета маршрутов на карте [feature]
    - **[feature]** Добавлена новая утилита `getFlightFeatures` для расчета маршрутов рейса, включая точку самолета, сплошной и пунктирный маршруты.
    - **[feature]** Обновлен компонент `MapComponent` для отображения активных и неактивных рейсов с использованием сплошных и пунктирных маршрутов.
    - **[feature]** Добавлен хук `useMapData` для получения данных маршрутов, включая неактивные рейсы.
    - **[feature]** Включены обновления координат аэропортов в `Flight.data.ts`.
    - **[feature]** Добавлена поддержка новых алиасов `@lib` в конфигурацию проекта (`vite.config.ts`, `tsconfig.json`).
    - **[feature]** Реализованы расчеты прогресса маршрута через новую утилиту `getMapRouteProgress`.

--- src/pages/Home/MapComponent/MapComponent.tsx
+++ src/pages/Home/MapComponent/MapComponent.tsx
@@ -1,24 +1,41 @@
-import Map, { Layer, type LayerProps, type MapRef, Source } from 'react-map-gl/maplibre';
-import type { Feature, FeatureCollection, Point, Position } from 'geojson';
-// import AirplaneIcon from '@assets/icons/other/airplane.svg?react';
+import Map, { Layer, type LayerProps, type MapRef, Marker, Source } from 'react-map-gl/maplibre';
+import type { Point } from 'geojson';
+import AirplaneIcon from '@assets/icons/other/airplane.svg?react';
 import { useEffect, useMemo, useRef } from 'react';
 import { useFlightSelectionState } from '@/hooks/useFlightSelectionState.ts';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import { AIRPORTS } from '@pages/Home/MapComponent/Airport.data.ts';
-import * as turf from '@turf/turf';
+import { useMapData } from '@/hooks/useMapData.ts';
+import { cn } from '@lib/utils.ts';
 
-const layerStylePath: LayerProps = {
+const layerStylePathSolid: LayerProps = {
 	id: 'path',
+	type: 'line',
+	source: 'geojson-active-source',
+	layout: {
+		'line-cap': 'round', // Rounded line caps
+		'line-join': 'round', // Rounded line joins
+	},
+	paint: {
+		'line-color': '#E68B02',
+		'line-width': 4,
+		'line-gradient': ['interpolate', ['linear'], ['line-progress'], 0, '#E68B02', 1, '#D24242'],
+	},
+};
+
+const layerStylePathDashed: LayerProps = {
+	id: 'path-dashed',
 	type: 'line',
 	source: 'geojson-active-source',
 	layout: {},
 	paint: {
-		'line-color': '#007cbf',
-		'line-width': 4,
+		'line-color': '#fff',
+		'line-width': 2,
+		'line-dasharray': [3, 4], // Dashed line style
+		'line-opacity': 0.7, // Optional: make the dashed line semi-transparent
 	},
 };
 
-const layerStylePoint: LayerProps = {
+/*const layerStylePoint: LayerProps = {
 	id: 'point',
 	type: 'circle',
 	source: 'geojson-active-source',
@@ -27,166 +44,48 @@ const layerStylePoint: LayerProps = {
 		'circle-color': '#007cbf',
 		'circle-radius': 10,
 	},
-};
-
-const layerStyleAircraft: LayerProps = {
-	id: 'point',
-	type: 'symbol',
-	source: 'geojson-active-source',
-	layout: {
-		'icon-image': 'airplane-icon', // Assuming you have an icon for the aircraft
-		'icon-rotate': ['get', 'bearing'],
-		'icon-rotation-alignment': 'map',
-		'icon-overlap': 'always',
-		'icon-ignore-placement': true,
-	},
-};
+};*/
 
 const mapStyle = '01980e13-49e3-7035-9cac-73918fb0bbba';
-
-type AirportCoordinates = {
-	longitude: number;
-	latitude: number;
-};
-
-type AirportData = {
-	[key: string]: AirportCoordinates;
-};
 
 const MapComponent = () => {
 	const mapRef = useRef<MapRef | null>(null);
 	const { currentFlight } = useFlightSelectionState();
+	const { aircraft, solidRoute, dashedRoute, inactiveFlights } = useMapData(currentFlight);
+	const { coordinates } = (aircraft?.features[0]?.geometry as Point) ?? [];
+	const aircraftBearing = (aircraft?.features[0]?.properties as { bearing?: number })?.bearing;
 
-	const airportPoints = useMemo<AirportData[] | null>(() => {
-		if (!currentFlight?.flight.from.airport || !currentFlight?.flight.to.airport) {
-			return null;
-		}
+	const inactiveFlightsMarker = useMemo(() => {
+		return inactiveFlights.map((fl, index) => {
+			const bearing = (fl?.aircraft?.features[0]?.properties as { bearing?: number })?.bearing ?? 0;
+			const coordinates = (fl?.aircraft?.features[0]?.geometry as { coordinates?: number[] })?.coordinates ?? [
+				0, 0,
+			];
 
-		const fromAirport = Object.entries(AIRPORTS).find(([code]) => code === currentFlight.flight.from.airport);
-		const toAirport = Object.entries(AIRPORTS).find(([code]) => code === currentFlight.flight.to.airport);
-		console.debug('[MapComponent] airport points', fromAirport, toAirport);
-
-		return [
-			{
-				[fromAirport?.[0] ?? '']: {
-					longitude: fromAirport?.[1].longitude ?? 0,
-					latitude: fromAirport?.[1].latitude ?? 0,
-				},
-			},
-			{
-				[toAirport?.[0] ?? '']: {
-					longitude: toAirport?.[1].longitude ?? 0,
-					latitude: toAirport?.[1].latitude ?? 0,
-				},
-			},
-		];
-	}, [currentFlight?.flight.from.airport, currentFlight?.flight.to.airport]);
-
-	const airportFeatures = useMemo<FeatureCollection | null>(() => {
-		console.debug('[MapComponent] airportPoints', airportPoints);
-
-		if (!airportPoints?.length) {
-			return null;
-		}
-
-		const features = airportPoints.map((airport) => {
-			const [code, { longitude, latitude }] = Object.entries(airport)[0];
-			console.debug('[MapComponent] airport coordinates', code, longitude, latitude);
-
-			return {
-				type: 'Feature',
-				geometry: {
-					type: 'Point',
-					coordinates: [longitude, latitude],
-				},
-				properties: {
-					code: code,
-					iconSize: [30, 30],
-					iconImage: 'airport-icon', // Assuming you have an icon for airports
-				},
-			} as Feature;
+			return (
+				<Marker
+					key={`inactive-flight-${index}`}
+					longitude={coordinates[0] ?? 0}
+					latitude={coordinates[1] ?? 0}
+				>
+					<div
+						className={cn('scale-75 opacity-70')}
+						style={{ rotate: `${bearing}deg` }}
+					>
+						<AirplaneIcon />
+					</div>
+				</Marker>
+			);
 		});
-		console.debug('[MapComponent] airport features', features);
-
-		return {
-			type: 'FeatureCollection',
-			features: features,
-		} as FeatureCollection;
-	}, [airportPoints]);
-
-	const pathCoordinates = useMemo<Position[]>(() => {
-		return (
-			airportFeatures?.features.map((feature) => {
-				const geometry = feature.geometry as Point;
-				console.debug('[MapComponent] path feature geometry', geometry);
-
-				return (
-					geometry?.coordinates ?? [
-						[0, 0],
-						[0, 0],
-					]
-				);
-			}) ?? [
-				[0, 0],
-				[0, 0],
-			]
-		);
-	}, [airportFeatures?.features]);
-
-	const pathDistance = useMemo(
-		() => (pathCoordinates ? turf.length(turf.lineString(pathCoordinates), { units: 'kilometers' }) : 0),
-		[pathCoordinates],
-	);
-
-	const pathDistanceCompleted = useMemo(() => {
-		return pathDistance * ((currentFlight?.route?.completedPercentage ?? 0) / 100);
-	}, [pathDistance, currentFlight?.route?.completedPercentage]);
-
-	const aircraftCoordinates = useMemo(
-		() =>
-			turf.along(
-				turf.lineString(
-					pathCoordinates ?? [
-						[0, 0],
-						[0, 0],
-					],
-				),
-				pathDistanceCompleted,
-				{ units: 'kilometers' },
-			),
-		[pathDistanceCompleted, pathCoordinates],
-	);
-
-	const geojsonActive = useMemo<FeatureCollection>(() => {
-		const coordinates = pathCoordinates ?? [
-			[0, 0],
-			[0, 0],
-		];
-		const result: FeatureCollection = {
-			type: 'FeatureCollection',
-			features: [
-				{
-					type: 'Feature',
-					geometry: {
-						type: 'LineString',
-						coordinates: coordinates,
-					},
-					properties: {},
-				},
-			],
-		};
-
-		return result;
-	}, [pathCoordinates]);
+	}, [inactiveFlights]);
 
 	// fly to the current flight if it is active
 	useEffect(() => {
 		console.debug('[MapComponent] current flight', currentFlight);
 
-		if (!currentFlight || !mapRef.current || !aircraftCoordinates) return;
+		if (!currentFlight || !mapRef.current || !coordinates) return;
 
-		console.debug('[MapComponent] aircraftCoordinates', aircraftCoordinates);
-		const { coordinates } = aircraftCoordinates.geometry;
+		console.debug('[MapComponent] aircraftCoordinates', coordinates);
 
 		mapRef.current.flyTo({
 			center: [coordinates[0], coordinates[1]], // [longitude latitude],
@@ -195,29 +94,60 @@ const MapComponent = () => {
 			curve: 1, // Adjust the curve of the flyTo animation
 			essential: true,
 		});
-	}, [aircraftCoordinates, currentFlight]);
+	}, [coordinates, currentFlight]);
 
 	return (
 		<Map
 			id="map"
 			ref={mapRef}
 			initialViewState={{
-				longitude: 69.9992665,
-				latitude: 41.3901475,
+				longitude: coordinates?.[0] ?? 131.8856,
+				latitude: coordinates?.[1] ?? 43.1155,
 				zoom: 6, // Initial zoom level
 			}}
+			reuseMaps
 			mapStyle={`https://api.maptiler.com/maps/${mapStyle}/style.json?key=${import.meta.env.VITE_MAP_API_KEY}`}
 			style={{ width: '100dvw', height: '100dvh', position: 'absolute', overflow: 'hidden' }}
 		>
-			<Source
+			{/*<Source
 				id="geojson-active-source"
 				type="geojson"
 				data={geojsonActive}
 			>
 				<Layer {...layerStylePath} />
 				<Layer {...layerStylePoint} />
-				<Layer {...layerStyleAircraft} />
-			</Source>
+			</Source>*/}
+			{solidRoute && (
+				<Source
+					id="geojson-active-source-solid"
+					type="geojson"
+					data={solidRoute}
+					lineMetrics
+				>
+					<Layer {...layerStylePathSolid} />
+				</Source>
+			)}
+			{dashedRoute && (
+				<Source
+					id="geojson-active-source-dashed"
+					type="geojson"
+					data={dashedRoute}
+				>
+					<Layer {...layerStylePathDashed} />
+				</Source>
+			)}
+			{aircraft && (
+				<Marker
+					longitude={coordinates[0]}
+					latitude={coordinates[1]}
+					anchor="center"
+				>
+					<div style={{ rotate: `${aircraftBearing ?? 0}deg` }}>
+						<AirplaneIcon />
+					</div>
+				</Marker>
+			)}
+			{...inactiveFlightsMarker}
 		</Map>
 	);
 };
